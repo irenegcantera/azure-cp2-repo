7c998d997f471f43f29620f40d53082ed3929f7f
Irene <66362460+irenegcantera@users.noreply.github.com>
2024-02-22 23:42:57 +0100

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -1,1 +1,14 @@
+# Creacion del recurso de grupo
+resource "azurerm_resource_group" "rg" {
+  name     = "myResourceGroup"
+  location = "westus2"
+}
 
+# Creacion del recurso ACR
+resource "azurerm_container_registry" "acr" {
+  name                = "contenedorRegistryPrueba1"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Premium"
+  admin_enabled       = false
+}
