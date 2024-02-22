341ba4ce2311ac2d39b9b51578bde0792b5cb52c
Irene <66362460+irenegcantera@users.noreply.github.com>
2024-02-22 23:43:48 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,15 @@
+# Configuracion proveedor Azure
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
 
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
