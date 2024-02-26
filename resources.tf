0cbfe9b443cda5781945b7ae4ccd40170745272e
Irene <66362460+irenegcantera@users.noreply.github.com>
2024-02-26 23:59:01 +0100

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -1,14 +1,79 @@
 # Creacion del recurso de grupo
 resource "azurerm_resource_group" "rg" {
+
   name     = "myResourceGroup"
   location = "westus2"
+
 }
 
 # Creacion del recurso ACR
 resource "azurerm_container_registry" "acr" {
+
   name                = "contenedorRegistryPrueba1"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Premium"
   admin_enabled       = false
+
+}
+
+# Creacion del recurso vm de linux
+resource "azurerm_virtual_network" "vnet" {
+
+  name                = "vnet1"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+}
+
+resource "azurerm_subnet" "subnet" {
+
+  name                 = "subnet1"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+
+}
+
+resource "azurerm_network_interface" "nic" {
+
+  name                = "vnic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "vm1"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = "admin"
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = "admin"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
 }
