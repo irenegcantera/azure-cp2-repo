bae63af229a3491770ef871f568b57aa37156cb9
Irene <66362460+irenegcantera@users.noreply.github.com>
2024-02-27 23:05:19 +0100

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -61,7 +61,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   admin_ssh_key {
     username   = "admin"
-    public_key = file("./.ssh/id_rsa.pub")
+    public_key = file("/home/admin/.ssh/id_rsa.pub")
   }
 
   os_disk {
