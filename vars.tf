e3050f7bc10b3d4563633aa96d9583cf318fc280
Irene <66362460+irenegcantera@users.noreply.github.com>
2024-02-28 22:46:42 +0100

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,47 +1,53 @@
 variable "name_rg" {
-  type = string
+  type        = string
   description = "Recurso de grupo de la infraestructura"
-  default = "myResourceGroup" 
+  default     = "myResourceGroup"
 }
 
 variable "location" {
-  type = string
+  type        = string
   description = "Región de Azure donde crearemos la infraestructura"
-  default = "westus2" 
+  default     = "westus2"
 }
 
 variable "name_arc" {
-  type = string
+  type        = string
   description = "Contenedor de azure registry de la infraestructura"
-  default = "contenedorRegistryPrueba1" 
+  default     = "contenedorRegistryPrueba1"
 }
 
 variable "name_vnet" {
-  type = string
+  type        = string
   description = "Red virtual de la infraestructura"
-  default = "vnet1" 
+  default     = "vnet1"
 }
 
 variable "name_subnet" {
-  type = string
+  type        = string
   description = "Subnet de la infraestructura"
-  default = "subnet1" 
+  default     = "subnet1"
 }
 
 variable "name_nic" {
-  type = string
+  type        = string
   description = "Interfaz de red de la infraestructura"
-  default = "vnic" 
+  default     = "vnic"
 }
 
 variable "name_vm" {
-  type = string
+  type        = string
   description = "Máquina virtual de la infraestructura"
-  default = "vm1" 
+  default     = "vm1"
 }
 
 variable "public_key_path" {
-  type = string
+  type        = string
   description = "Ruta para la clave pública de acceso a las instancias"
-  default = "/home/irene/.ssh/id_rsa.pub" 
+  default     = "/home/irene/.ssh/id_rsa.pub"
+}
+
+variable "ssh_user" {
+  type = string
+  description = "Usuario para hacer ssh"
+  default = "azureuser"
 }
